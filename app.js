f52cbbedbe4a1425e626adac706a3e98450f78fc
unknown <you@example.com>
2023-04-11 01:39:19 +0530

    first commit

---
+++ app.js
@@ -0,0 +1,75 @@
+const express = require("express");
+const bodyParser = require("body-parser");
+const request = require("request");
+const https = require("https");
+
+const app = express();
+
+app.use(express.static("public"));
+
+app.use(bodyParser.urlencoded({ extended: true }));
+
+app.get("/", function (req, res) {
+  res.sendFile(__dirname + "/signup.html");
+});
+
+app.post("/", function (req, res) {
+  const firstName = req.body.firstName;
+  const lastName = req.body.lastName;
+  const email = req.body.email;
+  // console.log(firstName + "___" + lastName);
+  var data = {
+    members: [
+      {
+        email_address: email,
+        status: "subscribed",
+        merge_fields: {
+          FNAME: firstName,
+          LNAME: lastName,
+        },
+      },
+    ],
+  };
+
+  const jsonData = JSON.stringify(data);
+
+  const url = "https://us21.api.mailchimp.com/3.0/lists/4389df792d";
+
+  const options = {
+    method: "POST",
+    auth: "Dheeraj:104f8e1b1010e6bdd65fc868b4287ee3-us21",
+  };
+
+  const request = https.request(url, options, function (response) {
+    if (response.statusCode === 200) {
+      res.sendFile(__dirname + "/success.html");
+    } else {
+      res.sendFile(__dirname + "/failure.html");
+    }
+
+    response.on("data", function (data) {
+      console.log(JSON.parse(data));
+    });
+  });
+
+  request.write(jsonData);
+  request.end();
+});
+
+app.post("/failure", function (req, res) {
+  res.redirect("/");
+});
+
+// app.post("/success", function (req, res) {
+//   res.redirect("https://github.com/D-zero-7");
+// });
+
+app.listen(process.env.PORT || 3000, function () {
+  console.log("The server is live at port 3000");
+});
+
+//api key
+//104f8e1b1010e6bdd65fc868b4287ee3-us21
+
+//audience id
+//4389df792d
